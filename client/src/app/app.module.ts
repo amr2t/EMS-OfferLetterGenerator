7e06806dd7cf7934ea834a6f26f3f1dfc3d37eac
isomraj <sraj@argusoft.com>
2023-02-08 01:55:17 +0530

    added nav bar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
