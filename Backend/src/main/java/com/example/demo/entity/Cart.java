998263a9b5b854d212d49d012ed4f58e42a3a680
Chandreshc <ccarpenter@argusoft.com>
2023-02-07 20:02:20 +0530

    Reference for backend to continue development

--- Backend/src/main/java/com/example/demo/entity/Cart.java
+++ Backend/src/main/java/com/example/demo/entity/Cart.java
@@ -23,7 +23,7 @@ import lombok.NoArgsConstructor;
 public class Cart {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.UUID)
 	private UUID id;
 	
 	@OneToMany(mappedBy="fromCart")
